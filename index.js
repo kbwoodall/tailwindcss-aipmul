4d3b5c1f1ae40cc5f0d5884593f2815b9fc05857
kbwoodall <kerry.woodall@gmail.com>
2021-12-06 12:22:14 -0800

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // Import stylesheets
 import './style.css';
 
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+//const appDiv = document.getElementById('app');
+
+const App = () => {
+  return (
+    <div >    
+      <p class="text-lg font-bold">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p> 
+       
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('app'));
