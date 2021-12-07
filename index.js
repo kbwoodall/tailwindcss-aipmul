adba97f6c3adf3e6d90f5e0302bb6ec714ec01bb
kbwoodall <kerry.woodall@gmail.com>
2021-12-06 22:21:31 -0800

    1 file modified

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ import './style.css';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { useState, useReducer, useEffect } from 'react';
+
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 
 const App = () => {
   return (
     <div class="flex justify-start bg-green-200 h-screen ">
-      <p class="text-sm font-bold m-10" >
-        “Tailwind CSS is the only framework that I've seen scale on large teams.
-        It’s easy to customize, adapts to any design, and the build size is
-        tiny.”
-      </p>
-      <p class="text-sm font-bold m-10" >
-        “Tailwind CSS is the only framework that I've seen scale on large teams.
-        It’s easy to customize, adapts to any design, and the build size is
-        tiny.”
-      </p>
-      
+      <div>
+        <p class="text-sm font-bold m-10">
+          <text class="text-blue-800">
+          “Tailwind CSS is the only framework that I've seen scale on large
+          teams. It’s easy to customize, adapts to any design, and the build
+          size is tiny.”
+          </text>
+        </p>
+   
+      </div>
+      <div>
+        <p class="text-sm font-bold m-10">
+        <text class="text-blue-800">
+          “Tailwind CSS is the only framework that I've seen scale on large
+          teams. It’s easy to customize, adapts to any design, and the build
+          size is tiny.”
+          </text>
+        </p>
+      </div>
     </div>
   );
 };
