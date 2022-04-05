e24b2b5b6da23c8bd0edfec942850edbb78615ec
kbwoodall <kerry.woodall@gmail.com>
2022-04-04 21:36:02 -0700

    1 file modified

--- index.js
+++ index.js
@@ -9,15 +9,36 @@ import { useState, useReducer, useEffect } from 'react';
 // Write Javascript code!
 //const appDiv = document.getElementById('app');alertme = () => {
 
+const logme = (props) => {
+  console.log(props.value);
+};
+
 const TextInput = (props) => {
+  const [text, setText] = useState('');
+  /*
+  <h2>How to use TextField Component in ReactJS?</h2>
+  <TextField
+    value={name}
+    label="Enter your name"
+    onChange={(e) => {
+      setName(e.target.value);
+    }}
+  />
+  <h3>Your Enter Value is: {name} </h3>
+</div>
+
+
+*/
+
   return (
     <div>
       <input
         type="text"
-        value={props.value}
-        onChange={(event) => console.log('value changed!')}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
       <p>Input again</p>
+      <h3>Your Enter Value is: {text} </h3>
     </div>
   );
 };
