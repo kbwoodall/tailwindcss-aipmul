171d7be4b65135b5a01b655ea361c224976e08dc
kbwoodall <kerry.woodall@gmail.com>
2022-07-03 00:12:40 -0700

    1 file modified

--- index.js
+++ index.js
@@ -63,6 +63,10 @@ const listing = () => {
     </p>
   ));
 
+  const printHold = printList.map((items) => (
+    console.log('hold ' + items.customer + items.item + items.cost)
+  ));
+
   const print = () => {
     for (let i = 0; i < list.length; i++) {
       tot = tot + list[0].cost;
@@ -85,6 +89,8 @@ const listing = () => {
         }
       }
     }
+
+    printHold;
 
     return printall(hcust, hitem, hcost);
   };
