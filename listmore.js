0a58773b7ef199a4382aa34fba2364e833599608
kbwoodall <kerry.woodall@gmail.com>
2023-09-25 20:33:19 -0700

    1 file modified

--- listmore.js
+++ listmore.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const listarg = 1;
+
 const List2 = () => {
   const pic1 =
     'https://stackblitz.com/files/tailwindcss-aipmul/github/kbwoodall/tailwindcss-aipmul/main/bird.png';
@@ -7,11 +9,18 @@ const List2 = () => {
   const pic2 =
     'https://stackblitz.com/files/tailwindcss-aipmul/github/kbwoodall/tailwindcss-aipmul/main/golfPants.jpg';
 
+  const nlist = [
+    { key: 1, value: { pic1 } },
+    { key: 2, value: { pic2 } },
+  ];
+
   const items = [pic1, pic2];
 
   return (
     <div className="bg-gray-100 mt-6">
-      <img class="flex justify-center items-center  w-64" src={pic1} />
+      <img class="flex justify-center items-center  w-64" 
+      src={pic2}/>
+     
     </div>
   );
 };
