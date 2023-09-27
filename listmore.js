bc8f2d4cac58c006b31939b0273751519f552014
kbwoodall <kerry.woodall@gmail.com>
2023-09-26 22:40:57 -0700

    1 file modified

--- listmore.js
+++ listmore.js
@@ -10,10 +10,10 @@ const List2 = () => {
     'https://stackblitz.com/files/tailwindcss-aipmul/github/kbwoodall/tailwindcss-aipmul/main/golfPants.jpg';
 
   const nlist = [
-    { key: 1, value: { pic1 } },
-    { key: 2, value: { pic2 } },
+    { key: 1, value: pic1 },
+    { key: 2, value: pic2 },
   ];
-
+  console.log('returning1 ' + pic2);
   let check = nlist.includes('key');
 
   const param = 1;
@@ -32,7 +32,7 @@ const List2 = () => {
 
   return (
     <div className="bg-gray-100 mt-6">
-      <img class="flex justify-center items-center  w-64" src={pic2} />
+      <img className="flex justify-center items-center  w-64" src={testObj} />
     </div>
   );
 };
